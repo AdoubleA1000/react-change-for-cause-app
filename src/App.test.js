b03868d6e287ce6ca266e6a042dce98d4144ee4f
AdoubleA1000 <64552889+AdoubleA1000@users.noreply.github.com>
2021-12-05 16:50:14 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,36 @@
 const App = require('./App');
 
 test('The admin email address should be correct', () => {
-    
+    function callback(data) {
+        expect(data).toBe('admin email correct')
+    }
+
+    fetchData(callback);
 });
 
 test('The admin password should be correct', () => {
+    function callback(data) {
+        expect(data).toBe('admin password correct')
+    }
+
+    fetchData(callback);
 
 });
 
 test('The admin is logged in', () => {
+    function callback(data) {
+        expect(data).toBe('admin login correct')
+    }
+
+    fetchData(callback);
 
 });
 
 test('The admin is logged out', () => {
+    function callback(data) {
+        expect(data).toBe('admin logout correct')
+    }
+
+    fetchData(callback);
 
 });
